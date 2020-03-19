c7cdd5573a51ce6d0edea02209b67f9e9f27b585
53xg0d <62362355+53xg0d@users.noreply.github.com>
2020-03-19 10:44:35 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+print('Do sljedeće provjere...')
+ 
+print('\n******************************************\n\nKrivi odgovor gasi program.')
+
+x1=input('Y/O (broj): ')
+if x1.lower()!='18':
+      exit()
+x2=input('Najveći grad u Srbiji: ')
+if x2.lower()!='zagreb' and x2.lower()!='vukovar':
+    exit()
+x3=input('Pobjednik Eurosonga (država): ')
+if x3.lower()!='litva' and x3.lower()!='rusija':
+     exit()
+x4=input('34 decimala korijena iz dva? ')
+if x4.lower()!='0':
+    exit()
+print('\n******************************************\n\nČestitam.')
+print('https://docs.google.com/document/d/1vKoxEXKQ9SbuObxFKeYCG-SGxrW6oEez89hRAbG8jeo/edit?usp=sharing')
+
+
+
+
+
+
